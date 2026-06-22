f031ed49e957a924c5af272dba60891124b09385
teja <gundaboinateja33@gmail.com>
2026-06-22 19:30:01 +0530

    added middle part

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const Header = () => {
   return (
+    <>
     <div className="Header">
-      <h2 style={{ color: 'red', marginBottom: '0.5rem' }}>Todo List</h2>
+      <h2 style={{ color: 'red', marginBottom: '0.5rem' ,paddingBottom:"60px"}}>Todo List</h2>
     </div>
+     <div className="Middler">
+      <h2 style={{ color: 'blue', marginBottom: '0.5rem' ,paddingBottom:"60px"}}>Have some Task</h2>
+    </div>
+    </>
   )
 }
